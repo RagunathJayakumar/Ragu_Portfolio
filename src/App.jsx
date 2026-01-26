f3afeeb212f827810bb9885c1400834f37b6ee3f
Ragunath06 <ragunath.j@codentrix.com>
2026-01-26 17:43:26 +0530

    this is the new commit changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>this is my Portfolio</h1>
-    </div>
+    <>
+      <div>
+        <h1 className='font-bold capitalize text-center mt-4 text-2xl' >this is my Portfolio</h1>
+
+      </div>
+    </>
   )
 }
 
