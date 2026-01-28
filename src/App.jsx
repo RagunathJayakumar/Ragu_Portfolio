c249fb6ec5a21657a720abcac817f708a6eacb10
Ragunath Jayakumar <146608347+RagunathJayakumar@users.noreply.github.com>
2026-01-28 22:47:57 +0530

    navbar-changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ const App = () => {
     <>
       <div>
         <h1 className='font-bold capitalize text-center mt-4 text-2xl' >this is my Portfolio</h1>
-        <h2 className='capitalize'>this ie me ragunath</h2>
       </div>
     </>
   )
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+
+
   return (
     <div>
-      
+      <ul>
+        <li>Home</li>
+        <li>Resume</li>
+        <li>Projects</li>
+        <li>contact</li>
+      </ul>
     </div>
   )
 }
